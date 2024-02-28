83ead0b44208952d8f3487023e22285e34f00e65
Keitumetse Sokwa <tsononel@gmail.com>
2024-02-28 12:30:49 +0200

    Serving html application with node and express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use((req, res) => {
+    res.status(404);
+    res.send('<h1>Error 404: Resource not found</h1>');
+})
+
+app.listen(3008, () => {
+    console.log("App is listening on port 3008")
+})
